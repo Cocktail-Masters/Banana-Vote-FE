c05c8945c6bc32496d99c6ecfb0feaf3c1f761da
mingyu <mingyu1218@naver.com>
2023-03-17 15:19:45 +0900

    feat: Recommend Box Slick으로 변경

--- src/components/home/RecommendBox.tsx
+++ src/components/home/RecommendBox.tsx
@@ -20,75 +20,28 @@ type recommendContents = {
 };
 
 type recommendBoxProps = {
-  recommendMode: boolean;
-  setRecommendMode: Dispatch<SetStateAction<boolean>>;
-  title: string;
   contents?: recommendContents[];
 };
 
-const RecommendBox = ({
-  recommendMode,
-  setRecommendMode,
-  title,
-  contents,
-}: recommendBoxProps) => {
+const RecommendBox = ({ contents }: recommendBoxProps) => {
   return (
-    <Box>
-      <CardHeader width={"100%"} height={"60px"}>
-        <Flex
-          position="relative"
-          justifyContent={"space-between"}
-          alignItems={"center"}
-          width="100%"
-        >
-          <ChevronLeftIcon
-            style={{ height: "28px", width: "auto", cursor: "pointer" }}
-            onClick={() => setRecommendMode(!recommendMode)}
-          />
-          <Heading
-            position="absolute"
-            top="50%"
-            left="50%"
-            transform="translate(-50%, -50%)"
-            as="h5"
-            size="md"
-            width="240px"
-            height="28px"
-            textAlign="center"
-            color={"blue.500"}
-          >
-            {title}
-          </Heading>
-          <ChevronRightIcon
-            style={{ height: "28px", width: "auto", cursor: "pointer" }}
-            onClick={() => setRecommendMode(!recommendMode)}
-          />
-        </Flex>
-      </CardHeader>
-      <Divider
-        width={"90%"}
-        borderColor="yellow.500"
-        borderWidth="1.5px"
-        marginX="auto"
-      />
-      <CardBody width={"100%"} height={"200px"}>
-        {contents &&
-          contents.map((content: recommendContents) => {
-            return (
-              <Box key={content.id} mb={2}>
-                <Link href={`/vote/detail/${content.id}`}>
-                  <Text
-                    noOfLines={1}
-                    fontWeight={"bold"}
-                    _hover={{ color: "blue.500", textDecoration: "underline" }}
-                  >
-                    {content.title}
-                  </Text>
-                </Link>
-              </Box>
-            );
-          })}
-      </CardBody>
+    <Box width={"100%"} height={"100%"}>
+      {contents &&
+        contents.map((content: recommendContents) => {
+          return (
+            <Box key={content.id} mb={2}>
+              <Link href={`/vote/detail/${content.id}`}>
+                <Text
+                  noOfLines={1}
+                  fontWeight={"bold"}
+                  _hover={{ color: "blue.500", textDecoration: "underline" }}
+                >
+                  {content.title}
+                </Text>
+              </Link>
+            </Box>
+          );
+        })}
     </Box>
   );
 };

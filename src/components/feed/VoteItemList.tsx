c11989eef04240bf88a04a71536c04020781635b
mingyu <mingyu1218@naver.com>
2023-03-20 18:29:11 +0900

    feat: 모바일 사이즈에서의 한 줄당 vote_item 갯수 수정

--- src/components/feed/VoteItemList.tsx
+++ src/components/feed/VoteItemList.tsx
@@ -3,14 +3,14 @@
  * @description 투표 항목들을 CArd 리스트 형태로 표시
  */
 
-import { Flex, Grid, GridItem } from "@chakra-ui/react";
-import React from "react";
+import { Flex, Grid, GridItem, useMediaQuery } from "@chakra-ui/react";
 import VoteItem from "./VoteItem";
 import { voteItemType } from "@/types";
 
-const ELEMENTS_PER_LINE = 4; // 한 줄에 표시될 요소 수
-
 const VoteItemList = ({ vote_items }: { vote_items: voteItemType[] }) => {
+  const [isLargerThan1200] = useMediaQuery(`(min-width: 960px)`);
+  const ELEMENTS_PER_LINE = isLargerThan1200 ? 4 : 2; // 한 줄에 표시될 요소 수
+
   return (
     <Flex justifyContent={"space-between"}>
       <Grid

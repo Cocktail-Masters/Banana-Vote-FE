3cce1383c735de53b2443ddc7d6acff314db9352
mingyu <mingyu1218@naver.com>
2023-03-23 23:10:24 +0900

    fix: 투표 생성 버튼 프로필 사진 디자인 오류 수정

--- src/components/common/BadgeImage.tsx
+++ src/components/common/BadgeImage.tsx
@@ -15,9 +15,6 @@ type BadgeProps = {
 const BadgeImage = ({ badge_url, nickname, isWriter }: BadgeProps) => {
   return (
     <>
-      {/* <Avatar name={!badge_url || badge_url.length < 1 ? undefined : nickname} src={badge_url}>
-				{isWriter && <AvatarBadge boxSize="1.25em" bg="green.500" />}
-			</Avatar> */}
       <Image
         className="w-10 h-10 rounded-full"
         src={badge_url ? badge_url : defaultProfile}
--- src/components/home/VoteCreateBar.tsx
+++ src/components/home/VoteCreateBar.tsx
@@ -20,19 +20,19 @@ const VoteCreateBar = ({ nickname, badge_url }: voteCreateBarProps) => {
   return (
     <div className="w-[90%] lg:w-[800px] m-auto h-[80px] mt-4 mb-4 bg-white rounded-xl drop-shadow-md">
       {/* 바디 */}
-      <div className="p-4 flex justify-between items-center h-full">
+      <div className="flex justify-between items-center h-full p-5">
         {/* 뱃지 */}
-        <div className="flex mr-2 h-full justify-center items-center">
-          <BadgeImage nickname={nickname} badge_url={badge_url} />
-        </div>
+        <BadgeImage nickname={nickname} badge_url={badge_url} />
         {/* 투표 생성 버튼 */}
-        <div className="flex w-full h-full justify-center items-center">
-          <Link className="w-full" href="/vote/create" passHref>
-            <button className="w-full h-10 bg-gray-100 rounded-full text-gray-500 font-semibold text-xs md:text-sm lg:text-md hover:bg-gray-200 transition">
-              원하는 투표를 만들어보세요
-            </button>
-          </Link>
-        </div>
+        <Link
+          className="flex w-full h-full justify-center items-center"
+          href="/vote/create"
+          passHref
+        >
+          <button className="ml-2 w-full h-10 bg-gray-100 rounded-full text-gray-500 font-semibold text-xs md:text-sm lg:text-md hover:bg-gray-200 transition">
+            원하는 투표를 만들어보세요
+          </button>
+        </Link>
       </div>
     </div>
   );

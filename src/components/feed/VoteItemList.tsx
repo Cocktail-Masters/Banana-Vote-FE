6633991ac82f86cb1d11d6e7ad698589a1459b48
mingyu <mingyu1218@naver.com>
2023-03-24 15:03:07 +0900

    fix: 그리드 미디어쿼리 적용 안되는 문제 해결

--- src/components/feed/VoteItemList.tsx
+++ src/components/feed/VoteItemList.tsx
@@ -5,38 +5,26 @@
  */
 import VoteItem from "./VoteItem";
 import { voteItemType } from "@/types";
-import { useEffect, useState } from "react";
 
 const VoteItemList = ({ vote_items }: { vote_items: voteItemType[] }) => {
-  const [colNum, setColNum] = useState<number>();
-
-  useEffect(() => {
-    setColNum(vote_items && vote_items.length < 4 ? vote_items.length : 4);
-  }, [vote_items]);
-
   return (
     <div className="flex justify-center select-none">
-      <div
-        className={`grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-${vote_items.length}`}
-      >
+      <div className={`grid gap-4 grid-cols-1 md:grid-cols-2`}>
         {vote_items &&
           vote_items.map((item: voteItemType, index: number) => {
             return (
               <div
-                className="w-full md:w-auto md:w-full truncate drop-shadow-md"
+                className={`w-full md:w-auto md:w-full truncate drop-shadow-md`}
                 key={index}
-                onClick={() => setColNum(5)}
               >
                 <VoteItem imageLink={item.image} content={item.title} />
               </div>
             );
           })}
       </div>
-      {/* TODO : 미디어쿼리 적용 안 되는 문제 해결하기 */}
       <style jsx>{`
-        .grid {
-          @media (min-width: 960px) {
-            background-color: blue;
+        @media (min-width: 960px) {
+          .grid {
             grid-template-columns: repeat(
               ${vote_items && vote_items.length < 4 ? vote_items.length : 4},
               minmax(0, 1fr)

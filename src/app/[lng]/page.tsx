1b933ec96cd71656d064f335061bec9ed0ca4bbf
JSC <gbs04087@gmail.com>
2023-03-24 14:44:44 +0900

    feat: add update pakcage

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -3,9 +3,9 @@ import { getDictionary } from "get-dictionary";
 import { Locale } from "i18n-config";
 
 export default async function Home({
-  params: { lang },
+  params: { lng },
 }: {
-  params: { lang: Locale };
+  params: { lng: Locale };
 }) {
   // const dictionary = await getDictionary(lang);
 

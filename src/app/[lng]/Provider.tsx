cac168545367ba7286729f3330bfcf9006f18c7b
JSC <gbs04087@gmail.com>
2023-03-27 15:09:49 +0900

    refactor: delete all chakra import

--- src/app/[lng]/Provider.tsx
+++ src/app/[lng]/Provider.tsx
@@ -1,14 +1,12 @@
 "use client";
 import React from "react";
-import theme from "@theme/theme";
-import { CacheProvider } from "@chakra-ui/next-js";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/common/reactQuery/QueryClient";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <CacheProvider>{children}</CacheProvider>
+      {children}
     </QueryClientProvider>
   );
 };

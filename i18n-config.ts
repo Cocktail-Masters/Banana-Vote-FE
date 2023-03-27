77b021fe8a26c1640486139d0760dd4b77257149
JSC <gbs04087@gmail.com>
2023-03-27 16:40:05 +0900

    feat: 국제화 마무리

--- i18n-config.ts
+++ i18n-config.ts
@@ -1,6 +1,25 @@
-export const i18n = {
-  defaultLocale: 'en',
-  locales: ['en', 'de', 'cs'],
-} as const
+import jp from "./language/jp.json";
+import en from "./language/en.json";
+import ko from "./language/ko.json";
 
-export type Locale = typeof i18n['locales'][number]
+export type dictionariesType = typeof dictionaries;
+export type Locale = keyof dictionariesType;
+
+export type transitionType = {
+  readonly locale: string;
+  readonly label: string;
+  readonly messages: Partial<typeof i18n.defaultLocale>;
+};
+
+// dictionaries에 언어를 추가하면 OK
+
+export const dictionaries = {
+  en: { locale: "en", label: "English", messages: en },
+  jp: { locale: "jp", label: "日本語", messages: jp },
+  ko: { locale: "ko", label: "한국어", messages: ko },
+};
+
+export const i18n = {
+  defaultLocale: "ko",
+  locales: Object.keys(dictionaries) as Array<Locale>,
+} as const;

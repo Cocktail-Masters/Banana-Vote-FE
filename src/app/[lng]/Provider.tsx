b1379b879ee306d3309d0e421c8c78be0832e22b
JSC <gbs04087@gmail.com>
2023-03-27 15:04:18 +0900

    refactor: delete chakra

--- src/app/[lng]/Provider.tsx
+++ src/app/[lng]/Provider.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "@theme/theme";
 import { CacheProvider } from "@chakra-ui/next-js";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -9,12 +8,7 @@ import { queryClient } from "@/common/reactQuery/QueryClient";
 const Provider = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
-        <CacheProvider>
-          {<ColorModeScript initialColorMode={theme.config.initialColorMode} />}
-          {children}
-        </CacheProvider>
-      </ChakraProvider>
+      <CacheProvider>{children}</CacheProvider>
     </QueryClientProvider>
   );
 };
--- src/common/reactQuery/QueryClient.ts
+++ src/common/reactQuery/QueryClient.ts
@@ -1,15 +1,8 @@
 import { QueryCache, QueryClient } from "@tanstack/react-query";
-import { createStandaloneToast } from "@chakra-ui/react";
-
-import theme from "@theme/theme";
-
-const { toast } = createStandaloneToast({ theme });
-
 const queryErrorHandler = (error: unknown): void => {
   const id = "react-query-error";
   const title =
     error instanceof Error ? error.message : "서버 연결에 문제가 발생했습니다.";
-  toast({ id, title, status: "error", variant: "subtle", isClosable: true });
 };
 
 export const queryClient = new QueryClient({

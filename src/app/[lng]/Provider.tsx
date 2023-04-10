4beb30b64a984d33e0756f4ac4eec1bac4e00bf2
mingyu <mingyu1218@naver.com>
2023-04-10 15:11:43 +0900

    chore: add=>react devtools, update=>nextjs13.3.0, 그 외 package.json 불필요한 패키지 정리

--- src/app/[lng]/Provider.tsx
+++ src/app/[lng]/Provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 type providerType = {
   children: React.ReactNode;
@@ -9,7 +10,10 @@ type providerType = {
 const Provider = ({ children }: providerType) => {
   const [queryClient] = useState(new QueryClient());
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
   );
 };
 

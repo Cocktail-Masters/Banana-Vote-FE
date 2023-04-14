94a996935f3c1658324a36a0c17324277312529f
mingyu <mingyu1218@naver.com>
2023-04-14 16:08:27 +0900

    feat: 메인페이지 다크모드 완료

--- src/components/home/RecommendArea.tsx
+++ src/components/home/RecommendArea.tsx
@@ -3,11 +3,11 @@
  * @description 메인페이지에서 인기 투표, 관심 투표 표시하는 영역
  */
 "use client";
-import RecommendBox from "./RecommendBox";
+import RecommendBox from "@/components/home/RecommendBox";
 import Slider from "react-slick";
 import { useState } from "react";
-import "./styles/slick.css";
-import "./styles/slick-theme.css";
+import "@/components/home/styles/slick.css";
+import "@/components/home/styles/slick-theme.css";
 import { usePopularListQuery } from "@/hooks/reactQuery/usePopularListQuery";
 import { useInterestListQuery } from "@/hooks/reactQuery/useInterestListQuery";
 import Loading from "@/components/Loading";
@@ -25,7 +25,7 @@ const RecommendArea = () => {
   });
 
   return (
-    <div className="relative mx-auto mt-4 mb-4 h-[260px] w-[360px] select-none rounded-xl bg-white drop-shadow-md">
+    <div className="relative mx-auto mt-4 mb-4 h-[260px] w-[360px] select-none rounded-xl bg-bg-feed drop-shadow-md dark:bg-bg-feed-dark">
       <div className="h-16 w-full">
         <div className="relative flex w-full items-center justify-center text-center">
           <div className="h-7 w-auto" />
@@ -36,7 +36,7 @@ const RecommendArea = () => {
         </div>
       </div>
       <hr className="mx-auto h-[1.5px] w-11/12 border-0 bg-yellow-500" />
-      <div className="h-52 w-full p-4">
+      <div className="h-52 w-full stroke-black p-4 text-text-article dark:stroke-white dark:text-text-article-dark">
         <Slider
           dots={true}
           infinite={false}
@@ -48,10 +48,10 @@ const RecommendArea = () => {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              strokeWidth={2}
               stroke="currentColor"
+              strokeWidth={2}
               id="slick-prev-arrow"
-              className="h-6 w-6 rounded-full p-4 hover:bg-slate-100"
+              className="h-6 w-6 rounded-full stroke-black p-4 text-black hover:bg-slate-100 dark:stroke-white dark:stroke-white"
             >
               <path
                 strokeLinecap="round"

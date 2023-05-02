2ce2d4f319dc2b99fc6fc7f26f84085d6d73cbc3
mingyu <mingyu1218@naver.com>
2023-05-02 17:50:03 +0900

    feat: 메인페이지 popular, interest api 연결

--- src/hooks/reactQuery/useInterestListQuery.ts
+++ src/hooks/reactQuery/useInterestListQuery.ts
@@ -3,23 +3,20 @@
  */
 
 import { useQuery } from "@tanstack/react-query";
-import { useInterestListDummy } from "@components/home/__test__/useInterestListDummy";
+import api from "@/common/axiosInstance";
 
 export const fetchInterestList = async () => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_HOSTNAME}/api/home/interest`
-  )
-    .then((response) => response.json())
-    .catch((e) => e);
+  try {
+    const res = await api.get(`/votes/interest`);
 
-  return response.res;
+    return res.data;
+  } catch (e: any) {
+    return e.response.data;
+  }
 };
 
 export const useInterestListQuery = ({ queryKey }: { queryKey: string }) => {
   return useQuery([queryKey], async () => {
-    // TODO : api 요청으로 데이터 받아오기
-    const response = useInterestListDummy;
-
-    return response;
+    return fetchInterestList();
   });
 };

c9f85fa65ab4f336e7b883550499a897bf660dc8
TypingOK <wjdwls717@gmail.com>
2023-05-04 16:26:59 +0900

    fix: 타입 에러 수정

--- src/app/api/pickets/[voteId]/route.ts
+++ src/app/api/pickets/[voteId]/route.ts
@@ -4,31 +4,31 @@ import { NextResponse } from "next/server";
 export const DummyPicket: picketsType = {
   pickets: [
     {
-      picket_image_url:
+      picketImageUrl:
         "https://cdn.discordapp.com/attachments/433506654009425921/987185259915399218/unknown.png",
       position: 1,
       price: 1600,
     },
     {
-      picket_image_url:
+      picketImageUrl:
         "https://cdn.discordapp.com/attachments/433506654009425921/1021418121933885460/unknown.png",
       position: 2,
       price: 2400,
     },
     {
-      picket_image_url:
+      picketImageUrl:
         "https://cdn.discordapp.com/attachments/433506654009425921/1021418121933885460/unknown.png",
       position: 3,
       price: 3600,
     },
     {
-      picket_image_url:
+      picketImageUrl:
         "https://cdn.discordapp.com/attachments/433506654009425921/1021418121933885460/unknown.png",
       position: 4,
       price: 4800,
     },
     {
-      picket_image_url:
+      picketImageUrl:
         "https://cdn.discordapp.com/attachments/433506654009425921/1021418121933885460/unknown.png",
       position: 5,
       price: 5000,

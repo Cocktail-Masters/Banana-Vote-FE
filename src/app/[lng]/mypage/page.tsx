0033515e7a517c5fe6d05533ebb793d8cb552128
TypingOK <wjdwls717@gmail.com>
2023-06-08 22:43:46 +0900

    feat: 마이페이지 카테고리 탭 추가

--- src/app/[lng]/mypage/page.tsx
+++ src/app/[lng]/mypage/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import InputNickname from "@/components/user/InputNickname";
 import SelectGender from "@/components/user/SelectGenderList";
 import SelectAge from "@/components/user/SelectAgeList";
@@ -5,13 +7,39 @@ import useChangeLanguagePath from "@/hooks/useChangeLanguagePath";
 
 import PageTitle from "@/components/common/PageTitle";
 import { getDictionary } from "get-dictionary";
+import CategoryArea from "@/components/store/CategoryArea";
+import { useState } from "react";
 
 const Mypage = ({ params: { lng } }: { params: { lng: string } }) => {
   const { messages } = getDictionary(lng);
+  const [currentCategory, setCurrentCategory] = useState<number>(0);
+  const MYPAGE_CATEGORIES = [
+    {
+      id: 0,
+      label: `${messages.mypage.category.profile}`,
+    },
+    {
+      id: 1,
+      label: `${messages.mypage.category.inventory}`,
+    },
+    {
+      id: 2,
+      label: `${messages.mypage.category.vote}`,
+    },
+    {
+      id: 3,
+      label: `${messages.mypage.category.point}`,
+    },
+  ];
 
   return (
     <div className="flex flex-col items-start justify-center xl:justify-between">
       <PageTitle title={messages.mypage.mypage} />
+      <CategoryArea
+        currentCategory={currentCategory}
+        setCurrentCategory={setCurrentCategory}
+        categories={MYPAGE_CATEGORIES}
+      />
       <InputNickname />
       <SelectGender></SelectGender>
       <SelectAge></SelectAge>

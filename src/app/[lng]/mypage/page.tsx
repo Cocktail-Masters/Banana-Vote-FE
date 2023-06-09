6f34a4b5498af871abb6f1593855edfa0cebc4a5
TypingOK <wjdwls717@gmail.com>
2023-06-09 23:24:08 +0900

    feat: 마이페이지 카테고리, 프로필 관련 기능 추가

--- src/app/[lng]/mypage/page.tsx
+++ src/app/[lng]/mypage/page.tsx
@@ -1,50 +1,13 @@
-"use client";
-
-import InputNickname from "@/components/user/InputNickname";
-import SelectGender from "@/components/user/SelectGenderList";
-import SelectAge from "@/components/user/SelectAgeList";
-import useChangeLanguagePath from "@/hooks/useChangeLanguagePath";
-
 import PageTitle from "@/components/common/PageTitle";
 import { getDictionary } from "get-dictionary";
-import CategoryArea from "@/components/store/CategoryArea";
-import { useState } from "react";
+import MypageSection from "./mypageSection";
 
 const Mypage = ({ params: { lng } }: { params: { lng: string } }) => {
   const { messages } = getDictionary(lng);
-  const [currentCategory, setCurrentCategory] = useState<number>(0);
-  const MYPAGE_CATEGORIES = [
-    {
-      id: 0,
-      label: `${messages.mypage.category.profile}`,
-    },
-    {
-      id: 1,
-      label: `${messages.mypage.category.inventory}`,
-    },
-    {
-      id: 2,
-      label: `${messages.mypage.category.vote}`,
-    },
-    {
-      id: 3,
-      label: `${messages.mypage.category.point}`,
-    },
-  ];
-
   return (
-    <div className='flex flex-col items-start justify-center xl:justify-between'>
+    <div className="flex flex-col items-start justify-center xl:justify-between">
       <PageTitle title={messages.mypage.mypage} />
-      <CategoryArea
-        currentCategory={currentCategory}
-        setCurrentCategory={setCurrentCategory}
-        categories={MYPAGE_CATEGORIES}
-      />
-      <InputNickname />
-      <div className='flex w-full justify-between'>
-        <SelectGender></SelectGender>
-        <SelectAge></SelectAge>
-      </div>
+      <MypageSection />
     </div>
   );
 };
